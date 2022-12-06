ed4bb0a1afedc29fdf19a82976acbf462737ed17
Brandon Rubio <brubio3724@gmail.com>
2022-12-06 13:29:29 -0500

    Completed Day 6 2022

---
+++ 2022/day6/part1.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from 'fs';
+
+const datastream = readFileSync('./2022/day6/input.txt', 'utf-8');
+
+let start = 0;
+let seen = new Set<string>();
+let total = 0;
+
+for (const c of datastream) {
+    while (seen.has(c)) {
+        seen.delete(datastream.charAt(start));
+        start += 1;
+    }
+
+    seen.add(c);
+    total += 1;
+
+    if (seen.size === 4) {
+        break;
+    }
+}
+
+console.log(total);
